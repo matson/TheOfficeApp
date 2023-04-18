6e13dcfe2710eab48e87944082669cc54e947eec
Madison <madison.k.adams@gmail.com>
2023-04-18 16:14:47 -0400

    first commit

---
+++ That's What Michael Scott Said/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  That's What Michael Scott Said
+//
+//  Created by Tracy Adams on 4/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

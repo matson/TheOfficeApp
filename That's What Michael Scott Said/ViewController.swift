dade41d7a7a23ad50e3b399b9736b3c17bf18d2a
Madison <madison.k.adams@gmail.com>
2023-04-20 12:23:19 -0400

    added gif successfully

--- That's What Michael Scott Said/ViewController.swift
+++ That's What Michael Scott Said/ViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        imageView.loadGif(name: "Office_Gif")
     }
 
 
